9f6362c43464b78d284a7300fab5bedf6fb93fff
JP-0506 <panchaljp73@gmail.com>
2026-02-04 15:54:37 +0530

    Added database module

---
+++ database/db.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+
+def get_db():
+
+    return psycopg2.connect(
+        dbname="CollabHub",  # your database name
+        user="postgres",  # your username
+        password="0506",  # your password
+        host="localhost",
+        port="5432",
+    )
