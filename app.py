5004ce452fa6bc2405742e07b6850b4548a6c530
JP-0506 <panchaljp73@gmail.com>
2026-02-08 10:44:41 +0530

    Refactor main application setup

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, session, url_for
 
 from auth import auth_bp
-
+from admin import admin_bp
 
 app = Flask(__name__)
 
@@ -9,9 +9,10 @@ app.secret_key = "collabhub_secret_key"
 
 
 # -----------------------------
-# Register Blueprint
+# Register Blueprints
 # -----------------------------
 app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(admin_bp, url_prefix="/admin")
 
 
 # -----------------------------
@@ -20,21 +21,6 @@ app.register_blueprint(auth_bp, url_prefix="/auth")
 @app.route("/")
 def home():
     return render_template("home.html")
-
-
-# -----------------------------
-# ADMIN DASHBOARD
-# -----------------------------
-@app.route("/admin")
-def admin_dashboard():
-
-    if "user_id" not in session:
-        return redirect(url_for("auth.login"))
-
-    if session.get("role") != "admin":
-        return "Access Denied ❌"
-
-    return render_template("admin/admin.html")
 
 
 # -----------------------------
@@ -49,7 +35,7 @@ def leader_dashboard():
     if session.get("role") != "project_leader":
         return "Access Denied ❌"
 
-    return render_template("projectLeader/dashboard.html")
+    return render_template("projectleader/dashboard.html")
 
 
 # -----------------------------
@@ -64,7 +50,7 @@ def employee_dashboard():
     if session.get("role") != "employee":
         return "Access Denied ❌"
 
-    return render_template("Employee/dashboard.html")
+    return render_template("employee/dashboard.html")
 
 
 # -----------------------------
@@ -74,6 +60,7 @@ def employee_dashboard():
 def logout():
 
     session.clear()
+
     return redirect(url_for("home"))
 
 
