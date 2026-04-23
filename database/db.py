738a32cc8a213ff5c19b8103a10c0bc9994726e2
Jaymin Panchal <panchaljp73@gmail.com>
2026-04-23 21:02:21 +0530

    Remove hardcoded database password from connection

--- database/db.py
+++ database/db.py
@@ -6,7 +6,7 @@ def get_db():
     return psycopg2.connect(
         dbname="CollabHub1",  # your database name
         user="postgres",  # your username
-        password="0506",  # your password
+        password="",  # your password
         host="localhost",
         port="5432",
     )
