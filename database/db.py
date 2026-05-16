8b92ecf5d051c7ab430f5a7c5bb121dc91dce3f4
JP-0506 <panchaljp73@gmail.com>
2026-05-16 22:56:39 +0530

    Configured database for production

--- database/db.py
+++ database/db.py
@@ -1,12 +1,31 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def get_db():
 
-    return psycopg2.connect(
-        dbname="CollabHub1",  # your database name
-        user="postgres",  # your username
-        password="",  # your password
-        host="localhost",
-        port="5432",
-    )
+    try:
+
+        # If DATABASE_URL exists (Render or .env)
+        database_url = os.getenv("DATABASE_URL")
+
+        if database_url:
+
+            return psycopg2.connect(database_url)
+
+        # Fallback for local PostgreSQL
+        return psycopg2.connect(
+            dbname="CollabHub1",
+            user="postgres",
+            password="0506",
+            host="localhost",
+            port="5432",
+        )
+
+    except Exception as e:
+
+        print("Database Connection Error:", e)
+
+        return None
